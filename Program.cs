9d94661f65d5f79b1110b6e5f711f79f9646f87c
Atish Ojha <ojhaatish11@gmail.com>
2022-01-11 13:45:54 +0545

    add two numbers

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c;
+            string astr, bstr, cstr;
+
+            a = 30; //assigning value
+            b = 45;
+
+            Console.WriteLine("Enter value of a ");
+            astr = Console.ReadLine();
+            Console.WriteLine("Enter value of b ");
+            bstr = Console.ReadLine();
+
+            //cstr = astr + bstr;
+
+            a = int.Parse(astr);
+            b = Convert.ToInt32(bstr);
+
+            c = a + b; //operation
+
+            Console.WriteLine("{0}+{1}={2}", a,b,c);
+            Console.ReadKey();
+
+        }
+    }
+}
